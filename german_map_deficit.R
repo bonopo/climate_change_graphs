2d23404227c8416fa26a8deca4b8c6028bce4f0f
bonopo <raphael.menke@posteo.de>
2020-07-14 20:33:49 +0200

    clean version with comments and clean code

--- german_map_deficit.R
+++ german_map_deficit.R
@@ -1,1 +1,15 @@
 #german map of deficit
+
+install.packages(c("rdwd", "geonames"))
+sapply(c("dplyr", "magrittr", "utils", "tidyverse", "lubridate", "RCurl", "RColorBrewer", "rdwd", "geonames"), require, character.only = T )
+
+setwd("t:/MAs/rmenke/r_gis_pro/r_projects/data/")
+setwd("C:/Users/Menke/Documents/Uni/R_practice/climate_change_graphs/")
+
+
+geonames::GNsearch(name="Berlin")
+
+
+indexFTP()
+
+nearbyStations(lat, lon, radius=50, mindate = , res = "daily", var=variable)
--- shiny_server.R
+++ shiny_server.R
@@ -1,44 +1,46 @@
 server <- function(input, output, session) {
- 
+  
+# search dwd station ------------------------------------------------------  
   search_dwd <- eventReactive(input$search, {
-    
     validate(
       need(input$lat != "", "Please enter a latitude"),
       need(input$lon != "", "Please enter a longitude"),
       need(input$rad != "", "Please enter a radius"),
       need(input$ref != "", "Please choose a reference period")
-
-       
     )
-    
-    withProgress(message = 'Searching nearby stations', value = 0.14, {
-        dwd.search(lat = input$lat, lon = input$lon,rad = input$rad, ref = input$ref)
-         })
+
+
+
+
+    withProgress(message = "Searching nearby stations", value = 0.14, {
+      dwd.search(lat = input$lat, lon = input$lon, rad = input$rad, ref = input$ref)
+    })
   })
-  
+
   output$search_result <- renderTable({
     search_dwd()
-    })
-  
-  #cumsum plot ####
-    
+  })
+
+
+
+# cumsum plot and table ---------------------------------------------------
+
+
   dwd_data <- eventReactive(input$create_plot, {
-    
     validate(
       need(input$year != "", "Please select a year"),
       need(input$ref != "", "Please select a refence period"),
       need(input$id != "", "Please select a Station id")
-      
     )
-    
-  
-      
-    
-    progress = Progress$new()
-    progress$set(message = "plotting", value=0)
-    
+
+
+
+
+    progress <- Progress$new()
+    progress$set(message = "plotting", value = 0)
+
     on.exit(progress$close())
-    
+
     updateProgress <- function(value = NULL, detail = NULL) {
       if (is.null(value)) {
         value <- progress$getValue()
@@ -46,40 +48,40 @@ server <- function(input, output, session) {
       }
       progress$set(value = value, detail = detail)
     }
-    
-    
-     dwd.cs.data(id = input$id, year = input$year, cnp = input$ref, updateProgress)
 
-   })
-  
-    output$ns_cum_sum_plot <- renderPlot({
-      plot.cs(dwd_data())
-    })
-       
-    output$ns_cum_sum_data <- renderTable({
-      table.cs(dwd_data())
-    })
 
-                
-                
- # monthly anomalies ####
+    dwd.cs.data(id = input$id, year = input$year, cnp = input$ref, updateProgress)
+  })
+
+  output$ns_cum_sum_plot <- renderPlot({
+    plot.cs(dwd_data())
+  })
+
+  output$ns_cum_sum_data <- renderTable({
+    table.cs(dwd_data())
+  })
+
+
+
+# monthly anomalies -------------------------------------------------------
+
   monthly_anomalies <- eventReactive(input$create_plot_anomalie, {
-    
     validate(
       need(input$year_anomalie != "", "Please select a year"),
       need(input$ref_anomalie != "", "Please select a refence period"),
       need(input$id_anomalie != "", "Please select a Station id")
-      
     )
-    
-  
-      
-    
-    progress = Progress$new()
-    progress$set(message = "plotting", value=0)
-    
+
+
+
+
+
+
+    progress <- Progress$new()
+    progress$set(message = "plotting", value = 0)
+
     on.exit(progress$close())
-    
+
     updateProgress <- function(value = NULL, detail = NULL) {
       if (is.null(value)) {
         value <- progress$getValue()
@@ -87,23 +89,16 @@ server <- function(input, output, session) {
       }
       progress$set(value = value, detail = detail)
     }
-    
-    
-     monthly.plot(id = input$id_anomalie, year = input$year_anomalie, cnp = input$ref_anomalie, updateProgress)
-   
-  }) 
-   
-   output$anomalies <- renderPlot({
-     precip.plot(monthly_anomalies())
-   })
 
-      output$anomalies2 <- renderPlot({
-     temp.plot(monthly_anomalies())
-   })
-     
-  
 
-    
+    monthly.plot(id = input$id_anomalie, year = input$year_anomalie, cnp = input$ref_anomalie, updateProgress)
+  })
+
+  output$anomalies <- renderPlot({
+    precip.plot(monthly_anomalies())
+  })
+
+  output$anomalies2 <- renderPlot({
+    temp.plot(monthly_anomalies())
+  })
 }
-
-
