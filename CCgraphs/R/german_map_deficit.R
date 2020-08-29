15a4284470512e1dc74db66aaae38a0647e611a7
bonopo <raphael.menke@posteo.de>
2020-08-29 17:32:24 +0200

    final working version fixed error of partial sums if stations stopped working after a certain month fixed error if the amount of stations now is larger/smaller thatn in the reference period

--- CCgraphs/R/german_map_deficit.R
+++ CCgraphs/R/german_map_deficit.R
@@ -5,6 +5,8 @@ library(lubridate)
 library(sf)
 library(dplyr)
 library(tidyverse)
+library(dichromat)
+
 
 # data(geoIndex)
 getwd()
@@ -12,6 +14,9 @@ load("./ws_data.RData")
 
 plot(lk[, "id_2"], reset = FALSE) # from package sf sf::plot doesn't work!!
 berryFunctions::colPoints("geoLaenge", "geoBreite", "Stationshoehe", data = m, add = T, legend = F)
+
+
+#refresch DWD DATA :
 
 
 
@@ -58,17 +63,17 @@ landkreis_rain <- function(lki, year_of_interest) # LandKreisIndex (row number i
 
   # checking if there is dwd stations in the landkreis and if there is reference data for ANY of the selected dwd stations. This is just a quick check a more detail checking is done in a later step. This is only to speed up the function, as it skips the whole function if there is ANY FALSE in the if statement
   id <- m[int[[lki]], "Stations_id"]
-  if(length(id) < 1){
+  def <- NA
+  if (length(id) < 1) {
     warning("No recent rainfall data available for Landkreis ", lki, ": ", lk$name_2[lki], call. = FALSE)
     out <- data.frame(paste0("x-", year_of_interest), NA, lki, NA)
     colnames(out) <- c("last_month", "number_of_stations", "lki", "def")
     return(out)
-  }else if(all(id %notin% ref_ids)){
-  warning("No historic rainfall data available for Landkreis ", lki, ": ", lk$name_2[lki], call. = FALSE)
+  } else if (all(id %notin% ref_ids)) {
+    warning("No historic rainfall data available for Landkreis ", lki, ": ", lk$name_2[lki], call. = FALSE)
     out <- data.frame(paste0("x-", year_of_interest), NA, lki, NA)
     colnames(out) <- c("last_month", "number_of_stations", "lki", "def")
     return(out)
-
   } else {
     # urls
     # TODO: m has to be incorporated in package as Dataset of all hourly precipitation recent DWD stations
@@ -77,17 +82,21 @@ landkreis_rain <- function(lki, year_of_interest) # LandKreisIndex (row number i
       res = "monthly", var = "kl", per = "r", outvec = TRUE
     )
 
-    clims <- dataDWD(urls, varnames = FALSE, dir = "../localdata", quiet = T, overwrite = TRUE, force = FALSE, progbar = 0)
+    clims <- dataDWD(urls, varnames = FALSE, dir = "../localdata", quiet = T, overwrite = TRUE, force = T, progbar = 0)
 
     # setting monthly mean
     if (length(urls) == 1) {
       rainmean <- clims$MO_RR
-      monthlyrain <- clims[c("MESS_DATUM", "MO_RR")] %>%
+      monthlyrain_temp <- clims[c("MESS_DATUM", "MO_RR")] %>%
         dplyr::mutate(date = lubridate::ymd(MESS_DATUM)) %>%
-        dplyr::filter(year(date) == year_of_interest) %>%
-        dplyr::summarise(sum(MO_RR, na.rm=F))
+        dplyr::filter(year(date) == year_of_interest)
 
 
+      last_month_with_data <- nrow(monthlyrain_temp) # get which momth is the last month with data
+
+
+      monthlyrain <- monthlyrain_temp %>%
+        dplyr::summarise(sum(MO_RR, na.rm = F))
     } else {
       # if more than 1 station
       # it makes a list of the stations with each two columns
@@ -104,14 +113,15 @@ landkreis_rain <- function(lki, year_of_interest) # LandKreisIndex (row number i
         dplyr::mutate(date = lubridate::ymd(MESS_DATUM)) %>%
         dplyr::filter(year(date) == year_of_interest)
 
-      last_month_with_data <- month(monthlyrain_temp$date[nrow(monthlyrain_temp)]) # get which momth is the last month with data
+      last_month_with_data <- nrow(monthlyrain_temp) # get which momth is the last month with data
 
       monthlyrain <- monthlyrain_temp %>%
         dplyr::select(tidyr::starts_with("monthly")) %>%
         dplyr::summarise_all(sum, na.rm = F)
+    }
+    colnames(monthlyrain) <- id
 
-}
-      colnames(monthlyrain) <- id
+    if (last_month_with_data > 0) {
 
       # adding reference (ref) stations
       # depending if the user wants the whole year or just the months  'sum-till-now' there has to be two different calculation methods
@@ -119,7 +129,7 @@ landkreis_rain <- function(lki, year_of_interest) # LandKreisIndex (row number i
         data_ref_subset <- data_ref %>%
           dplyr::filter(Stations_id %in% id) %>% # getting the correct reference strations
           dplyr::group_by(Stations_id) %>%
-          dplyr::select(Stations_id,Jahr) %>% # taking the cumulative sum of the whole year of the reference stations
+          dplyr::select(Stations_id, Jahr) %>% # taking the cumulative sum of the whole year of the reference stations
           dplyr::summarise_all(mean) %>% # getting mean over all reference period per reference station
           t() %>%
           as.data.frame()
@@ -140,29 +150,43 @@ landkreis_rain <- function(lki, year_of_interest) # LandKreisIndex (row number i
           as.data.frame()
       }
 
-      colnames(data_ref_sum) <- id
+
+      # giving the ref station the correct id name
+      colnames(data_ref_sum) <- data_ref_subset$Stations_id
+
+      # if reference and year of interest do not have the same station id, only the data is used where BOTH is available: current AND reference, all other are thrown out. This doesn't make up for differences caused between reference and current by moving of the station etc...
+      common_cols <- intersect(colnames(monthlyrain), colnames(data_ref_sum))
 
       # deficit/surplus calculation
-      deficit_temp <- rbind(monthlyrain, data_ref_sum)
+      deficit_temp <- rbind(
+        monthlyrain[common_cols],
+        data_ref_sum[common_cols]
+      )
+
       deficit <- deficit_temp[1, ] / deficit_temp[2, ]
       number_of_stations <- length(which(!is.na(deficit)))
 
-      if(length(urls) == 1){
-        def <- round(deficit* 100, 0)
-      }else{
+      if (length(deficit) == 1) {
+        def <- round(deficit * 100, 0)
+      } else {
         def <- round(rowMeans(deficit[1, ], na.rm = T) * 100, 0)
-
       }
-      result <- cbind.data.frame(
-        "last_month" = paste0(last_month_with_data, "-", year_of_interest),
-        number_of_stations,
-        lki,
-        def
-      )
+
+      # removing 0
+      if (def == 0 && !is.na(def)) {
+        def <- NA
+      }
     }
-    print(lki)
-    return(result)
+    result <- cbind.data.frame(
+      "last_month" = paste0(last_month_with_data, "-", year_of_interest),
+      number_of_stations,
+      lki,
+      def
+    )
   }
+
+   return(result)
+}
 
 
 
@@ -170,62 +194,60 @@ landkreis_rain <- function(lki, year_of_interest) # LandKreisIndex (row number i
 # styler::style_file(path = "C:/Users/Menke/Documents/Uni/R_practice/climate_change_graphs/shiny_app/cc_graph/climate_change_graphs__vJR/CCgraphs/R/german_map_deficit.R")
 
 
-rainLK <- pbapply::pbsapply(c(1:nrow(lk)), 2020, FUN = landkreis_rain)
+rainLK <- pbapply::pblapply(c(1:nrow(lk)), 2020, FUN = landkreis_rain)
 
-landkreis_rain(lki = 9, 2020)
+landkreis_rain(lki = 1, 2020)
 
 # rainLK is a large list with all stations with the recent rain data (since 12-2018). It is ordered by rows of the Landkreise i.e. the first element in the list is also the first row in the lk table.
 
-# writing raindata to sf object to be able to print
-# print map ####
-list_result <- rainLK
-def.map_germany <- function(list_result = rainLK, map_data = lk) {
-  df_rainLK <- do.call("rbind", list_result)
+df_rainLK <- do.call("rbind", rainLK)
 
-  df_rainLK$anamoly_61 <- round(100 * df_rainLK$int_sum / df_rainLK$ref_station_61, 0)
-  df_rainLK$anamoly_71 <- round(100 * df_rainLK$int_sum / df_rainLK$ref_station_71, 0)
-  df_rainLK$anamoly_81 <- round(100 * df_rainLK$int_sum / df_rainLK$ref_station_81, 0)
+# removing LKI with only partly data (i.e. stopped measuring after month x)
 
-  for (i in 1:length(int)) {
-    rain_per_lk <- df_rainLK %>%
-      dplyr::select()
-  }
+remove <- which(df_rainLK$last_month != raster::modal(df_rainLK$last_month))
+df_rainLK$def[remove] <- NA
 
-  print({
-    plot(lk[, "rain_deficit"], reset = TRUE) # from package sf sf::plot doesn't work!!
-  })
+
+lk[1,]
+
+lk$rain_deficit <- df_rainLK$def
+
+
+colour_palete <- dichromat::colorschemes$DarkRedtoBlue.18
+# (max(lk$rain_deficit, na.rm = T)-min(lk$rain_deficit, na.rm=T))
+which.min(lk$rain_deficit)
+
+if ((100 - min(lk$rain_deficit, na.rm = T)) > (max(lk$rain_deficit, na.rm = T) - 100)) {
+  limits_scale <- c(min(lk$rain_deficit, na.rm = T), 100 + (100 - min(lk$rain_deficit, na.rm = T)))
+} else {
+  limits_scale <- c(100 - (max(lk$rain_deficit, na.rm = T) - 100), max(lk$rain_deficit, na.rm = T))
 }
 
-# 1step
-
-# 2step get the cumulative rain amount per landkreis for the current year
 
 
+myPalette <- colorRampPalette(brewer.pal(11, "RdBu"))
+sc <- scale_fill_gradientn('Niederschlagsdefizit [%]',
+  colours = myPalette(100),
+  limits = limits_scale
+)
 
-plot(lk[, "rain_deficit"], reset = TRUE) # from package sf sf::plot doesn't work!!
+# plot(lk[, "rain_deficit"], reset = TRUE) # from package sf sf::plot doesn't work!!
+ggplot() +
+  geom_sf(data = lk, aes(fill = rain_deficit)) +
+  sc +
+ theme_bw()
 
-head(lk)
-# old####
-#
-#
-# data("metaIndex")
-# m <- metaIndex
-# m <- m[m$res == "monthly" & m$var == "kl" & m$per == "recent" & m$hasfile, ]
-#
-# head(m)
-# msf <- sf::st_as_sf(m, coords = c("geoLaenge", "geoBreite"), crs = 4326)
-# # Read district shapefile
-# # downloaded from https://public.opendatasoft.com/explore/dataset/landkreise-in-germany/export/
-# # TODO: incorporate into package (Justus?)
-# lk <- sf::st_read("../../landkreise-in-germany.shp")
-#
-# # intersections: list with msf rownumbers for each district getting all DWD Id that measure rain recently in  each Landkreis
-# int <- sf::st_intersects(lk, msf)
-#
-# plot(lk[, "id_2"], reset = FALSE)
-# berryFunctions::colPoints("geoLaenge", "geoBreite", "Stationshoehe", data = m, add = T, legend = F)
-#
-# nrow(lk)
-#
+ggsave(filename = 'deficit.png', units = 'cm', device = 'png', width = 20, height = 30)
+
+
+  scale_fill_gradient2(
+    high = "#2400D9",
+    mid = "white",
+    low = "#e34a33",
+    midpoint = 100,
+    na.value = "grey50"
+  ) +
+  geom_blank()
+
 
 save.image(file = "ws_data.RData")
